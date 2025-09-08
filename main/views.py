5971ed74a513cb104e0c780aa303c3905c0e7a57
Cyaside <tristan.rasheedsatria@gmail.com>
2025-09-08 13:49:29 +0700

    Changes

--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ def home(request):
 	context = {
 		'app_name': '90minutesZone',
 		'nama': 'Tristan Rasheed Satria',  
+		'npm': '2406358472',
 		'kelas': 'PBP-C' 
 	}
 	return render(request, 'main/home.html', context)
