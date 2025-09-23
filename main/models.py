a949de3adb2d9b58230503f91f998da8740a591b
Cyaside <tristan.rasheedsatria@gmail.com>
2025-09-23 20:32:00 +0700

    Complete Tugas 4 Implementasi Autentikasi, Session, dan Cookies pada Django

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from django.contrib.auth.models import User
 
 
 class Product(models.Model):
@@ -22,6 +23,7 @@ class Product(models.Model):
     is_featured = models.BooleanField(default=False)
     views = models.PositiveIntegerField(default=0)
     # brand = models.CharField(max_length=100, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
