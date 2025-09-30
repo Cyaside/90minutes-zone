e7f22ca3f913fee2d69636398560ae5d1cbbcfaf
Cyaside <tristan.rasheedsatria@gmail.com>
2025-10-01 02:04:09 +0700

    Fix : static files

--- README.md
+++ README.md
@@ -24,5 +24,6 @@ https://tristan-rasheed-footballnews.pbp.cs.ui.ac.id/
 ## ‼️ Markdown
 
 1️⃣ [Tugas 2 Markdown](markdowns/tugas2.md)  
-2️⃣ [Tugas 3 Markdown](markdowns/tugas3.md)
-3️⃣ [Tugas 4 Markdown](markdowns/tugas4.md)
+2️⃣ [Tugas 3 Markdown](markdowns/tugas3.md)  
+3️⃣ [Tugas 4 Markdown](markdowns/tugas4.md)  
+4️⃣ [Tugas 5 Markdown](markdowns/tugas5.md)
--- minutes90_zone/settings.py
+++ minutes90_zone/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'minutes90_zone.urls'
@@ -140,10 +141,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
+# Dokumentasi: https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
